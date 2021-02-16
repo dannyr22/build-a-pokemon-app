daded723e0311684c67873f00950a378c5aaae93
dannyr22 <dannyr2763@hotmail.com>
2021-02-16 08:46:25 +0000

    pagination setup and api working properly, just need to style

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
 import { useState, useEffect } from 'react'
 import PokemonList from './PokemonList';
+import Pagination from './Pagination'
 import axios from 'axios'
 
 function App() {
   const [pokemon, setPokemon] = useState([])
+  const [currentPageUrl, setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon/')
+  const [nextPageUrl, setNextPageUrl] = useState("https://pokeapi.co/api/v2/pokemon?offset=20&limit=20")
+  const [previousPageUrl, setPreviousPageUrl] = useState("https://pokeapi.co/api/v2/pokemon?offset=0&limit=20")
+  const [loading, setLoading] = useState(true)
 
 
   useEffect(() => {
+  setLoading(true)
+  let cancel
+    axios.get(currentPageUrl, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    })
+  .then(res => {
+    setLoading(false)
+    setNextPageUrl(res.data.next)
+    setPreviousPageUrl(res.data.previous)
+    setPokemon(res.data.results.map(p => p.name))
+  })
+  return () => cancel()
+  }, [currentPageUrl])
 
-    axios.get('https://pokeapi.co/api/v2/pokemon/')
-  .then(res => setPokemon(res.data.results.map(p => p.name)))
-  }, [])
+  function goToNextPage(){
+    setCurrentPageUrl(nextPageUrl)
+  }
+  function goToPreviousPage(){
+    setCurrentPageUrl(previousPageUrl)
+  }
+
+
   
  
-
+if (loading) return "Loading..."
 
   return (
-   <PokemonList pokemon={pokemon}/>
+    <>
+      <PokemonList pokemon={pokemon}/>
+      <Pagination 
+        goToNextPage={nextPageUrl ? goToNextPage : null}
+        goToPreviousPage={previousPageUrl ? goToPreviousPage : null}
+      />
+    </>  
   );
 }
 
--- src/Pagination.js
+++ src/Pagination.js
@@ -1,5 +1,10 @@
-const Pagination = () => {
-    return (  );
+const Pagination = ({goToNextPage, goToPreviousPage}) => {
+    return (
+        <>
+            {goToPreviousPage && <button onClick={goToPreviousPage}>Previous</button>}
+            {goToNextPage && <button onClick={goToNextPage}>Next</button>}
+        </>
+    );
 }
  
 export default Pagination;
